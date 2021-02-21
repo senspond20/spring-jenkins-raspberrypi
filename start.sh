4fc4e8bbf473357ecec169aba09dfeb0b0aafe58
senspond20 <senspond@gmail.com>
2021-02-21 21:21:37 +0900

    deploy jenkins test2

--- start.sh
+++ start.sh
@@ -8,7 +8,8 @@ SERVER_PORT=8080
 PROJECT_PATH=/var/lib/jenkins/workspace/$PROJECT_NAME/target
 JAR_FILE=$PROJECT_PATH/$BUILD_NAME.jar
 
-sudo nohup java -jar $JAR_FILE 
+sudo nohup java -jar $JAR_FILE  --spring.profiles.active=dev > /dev/null 2>&1 &
+
 
 sudo echo "java -jar $JAR_FILE start on $SERVER_PORT"
 sudo echo "RUN!!"
